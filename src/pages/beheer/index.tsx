6a57816be59a353ace3097390e2fe9e0d479728f
Marc Buma <mosbuma@bumos.nl>
2025-07-08 13:31:44 +0200

    cleanup

--- src/pages/beheer/index.tsx
+++ src/pages/beheer/index.tsx
@@ -1,18 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { type GetServerSidePropsContext } from 'next';
-import { type ReportBikepark } from '../../components/beheer/reports/ReportsFilter';
-import { type contacts } from '@prisma/client';
-import { useSession } from 'next-auth/react';
 import BeheerPage, { getServerSideProps as importedGetServerSideProps } from './[activecomponent]/index';
 
 export const getServerSideProps = async (_props: GetServerSidePropsContext) => {
   return importedGetServerSideProps(_props);
 }
 
-const DefaultBeheerPage: React.FC<{gemeenten?: contacts[], bikeparks?: ReportBikepark[]}>= () => {
-  const { data: session } = useSession()
-
-  return <BeheerPage currentUser={session?.user} />;
+const DefaultBeheerPage: React.FC<{}>= () => {
+  return <BeheerPage />;
 };
 
 export default DefaultBeheerPage;
