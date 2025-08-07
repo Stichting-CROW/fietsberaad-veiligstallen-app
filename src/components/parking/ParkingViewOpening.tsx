31b345aeac8d59b2589e93e77462c193a50d802d
bartwr <mail@bartroorda.nl>
2025-08-07 11:09:33 +0200

    Fix duplicate import

--- src/components/parking/ParkingViewOpening.tsx
+++ src/components/parking/ParkingViewOpening.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { formatOpeningTimes, hasCustomOpeningTimesComingWeek } from "~/utils/parkings-openclose";
 import moment from "moment";
 
-import { ParkingDetailsType, UitzonderingOpeningstijden } from "~/types/parking";
 import { type ParkingDetailsType, UitzonderingOpeningstijden } from "~/types/parking";
 
 import SectionBlock from "~/components/SectionBlock";
