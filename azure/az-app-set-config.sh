250f46c2bb3a4fc0f22753c377c5a343de1ddeed
bartwr <mail@bartroorda.nl>
2025-10-21 17:12:20 +0200

    WIP File upload: Disable Azure changes to fix ACC deploym.

--- azure/az-app-set-config.sh
+++ azure/az-app-set-config.sh
@@ -1,11 +1,19 @@
+# set -o allexport; source .env; set +o allexport
+
+# # 
+# az webapp config set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --startup-file "npm start"
+
+# # Configure persistent storage for uploads directory
+# az webapp config appsettings set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
+
+# echo "Azure Web App configuration completed."
+# echo "Note: The uploads directory will be created automatically when the first file is uploaded."
+
+
+
+
 set -o allexport; source .env; set +o allexport
 
 # 
-az webapp config set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --startup-file "npm start"
-
-# Configure persistent storage for uploads directory
-az webapp config appsettings set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
-
-echo "Azure Web App configuration completed."
-echo "Note: The uploads directory will be created automatically when the first file is uploaded."
+az webapp config set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --startup-file "node server.js"
 
