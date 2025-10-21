82bdd138a96dc206bf41a015abeeb9a94cd02ca7
bartwr <mail@bartroorda.nl>
2025-10-21 17:00:43 +0200

    TMP File upload: Try to upload to /uploads

--- azure/az-app-set-config.sh
+++ azure/az-app-set-config.sh
@@ -1,7 +1,7 @@
 set -o allexport; source .env; set +o allexport
 
 # 
-az webapp config set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --startup-file "node server.js"
+az webapp config set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --startup-file "npm start"
 
 # Configure persistent storage for uploads directory
 az webapp config appsettings set --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
