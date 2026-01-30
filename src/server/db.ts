7ea2d128f9210e4d3f0a9843b51eb4e54bbe9154
Marc Buma <mosbuma@bumos.nl>
2026-01-30 09:23:34 +0100

    zelde werking prisma singleton op PROD en ACC/DEV

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "~/generated/prisma-client";
-
 import { env } from "~/env.mjs";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    // log: env.NODE_ENV === "development" ? ["error", "warn"] : ["error"], // "query" -> add to development to see queries
+// Only create and store if it doesn't exist yet
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = new PrismaClient({
     log: ["error"],
   });
+}
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Always export the singleton instance
+export const prisma = globalForPrisma.prisma;
