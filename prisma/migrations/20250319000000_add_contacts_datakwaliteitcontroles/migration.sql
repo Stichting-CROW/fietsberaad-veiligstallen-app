43604b6bc3d7e7b911715e542d159f06912f1089
bartwr <mail@bartroorda.nl>
2026-03-19 16:36:02 +0100

    fix: Fix failing migration for adding contacts_datakwaliteitcontroles

--- prisma/migrations/20250319000000_add_contacts_datakwaliteitcontroles/migration.sql
+++ prisma/migrations/20250319000000_add_contacts_datakwaliteitcontroles/migration.sql
@@ -9,7 +9,7 @@ CREATE TABLE `contacts_datakwaliteitcontroles` (
     INDEX `contacts_datakwaliteitcontroles_user_id_idx`(`user_id`),
     INDEX `contacts_datakwaliteitcontroles_createdAt_idx`(`createdAt`),
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+);
 
 -- AddForeignKey
 ALTER TABLE `contacts_datakwaliteitcontroles` ADD CONSTRAINT `contacts_datakwaliteitcontroles_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
